7186107dd78664065762e4da399c074010062e07
Adam Bowen <adam.bowen@delphix.com>
2019-10-24 12:38:02 -0400

    changes to add notes field

---
+++ app_changes/notes_changes/src/main/java/com/delphix/daf/model/Patient.java
@@ -0,0 +1,24 @@
+package com.delphix.daf.model;
+
+import lombok.*;
+import javax.persistence.*;
+
+@Entity
+@Getter @Setter
+@NoArgsConstructor
+@Table(name = "patients")
+public class Patient extends AuditModel {
+    @Id @GeneratedValue
+    private Long id;
+    private @NonNull String firstname;
+    private @NonNull String middlename;
+    private @NonNull String lastname;
+    private @NonNull String ssn;
+    private @NonNull String dob;
+    private @NonNull String address1;
+    private @NonNull String address2;
+    private @NonNull String city;
+    private @NonNull String state;
+    private @NonNull String zip;
+    private String notes;
+}
